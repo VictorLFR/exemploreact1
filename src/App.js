f7f18bce71733da7ce1fe9f1ee000819f18d5a5b
VictorLFR <44615983+VictorLFR@users.noreply.github.com>
2019-06-10 16:10:22 -0300

    Melhor apresentação ao publico

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Teste no Heroku, parece OK!
+          Edit <code>src/App.js</code> and save to reload. Mateuzinho coisa braba!
         </p>
         <a
           className="App-link"
